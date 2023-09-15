77d8c0f8557aa8ddf28cb46764410e6519500ab1
DHIRAJkumar80023 <137038587+DHIRAJkumar80023@users.noreply.github.com>
2023-09-15 12:41:37 +0530

    my facebook clone
    facebook close using react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Post from "./components/post/Post";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {BrowserRouter ,Routes , Route , Navigate} from "react-router-dom";
+
+
+
 
 function App() {
+  const user = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/login" element={ user ? <Navigate to="/"/> : <Login/>}/>
+       <Route path="/register" element={<Register/>}/>
+       <Route path="/profile/:id" element={user ? <Profile/> : <Navigate to="/login"/> }/>
+    </Routes>
+    
+    </BrowserRouter>
+    
+
+    
   );
 }
 
